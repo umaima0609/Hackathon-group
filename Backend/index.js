def03f82504d7447a285ecd09cd27096666bf719
Umaima Khan <umaimakh0609.com>
2024-08-25 17:31:51 +0500

    Deployment error resolve

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use(cors({
   
-  origin:  'http://localhost:5173',
+  origin:  'https://smit-student-feedbacker.vercel.app/',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }));
